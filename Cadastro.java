9a83228d7dabe3b98dc4ffe816e7360d65138bf5
Andressa Farkas <48599122+andressafarkas@users.noreply.github.com>
2022-05-23 18:49:03 -0300

    Update Cadastro.java

--- Cadastro.java
+++ Cadastro.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
-import java.util.Scanner;
 public class Cadastro {
+    
     public static void main (String[] args){
         
         Scanner s = new Scanner(System.in);
